915d59d483a5ef4c4ef58158f140709081136b77
Rosen Sasov <rosen.sasov@mail.com>
2019-11-02 18:08:58 +0200

    Add documentation

--- module03/solution.py
+++ module03/solution.py
@@ -1,4 +1,9 @@
 def sum_of_digits(number):
+    """
+    Calculates the sum of the digits of number
+    :param number: input number
+    :return: sum of the digits of number
+    """
     sum = 0
     number = abs(number)
     while number > 0:
@@ -8,6 +13,12 @@ def sum_of_digits(number):
 
 
 def to_digits(number):
+    """
+    Calculates the digits of the number
+    and return them as list
+    :param number: input number
+    :return: list of the digits of number
+    """
     digits = []
     while number > 0:
         digits.append(number % 10)
@@ -17,6 +28,11 @@ def to_digits(number):
 
 
 def to_number(digits):
+    """
+    Returns number containing the input digits
+    :param digits: list of digits
+    :return: number formed with the input digits
+    """
     number = 0
     digits.reverse()
     for digit_index in range(0, len(digits)):
@@ -25,16 +41,33 @@ def to_number(digits):
 
 
 def count_vowels(string):
+    """
+    Calculates the count of all vowels in the string
+    :param string: input string
+    :return: count of the vowels in the string
+    """
     vowels = "aeiouy"
     return calculate_char_occurrences_in_str(vowels, string)
 
 
 def count_consonants(string):
+    """
+    Calculates the count of all consonants in the string
+    :param string: input string
+    :return: count of the consonants in the string
+    """
     consonants = "bcdfghjklmnpqrstvwxz"
     return calculate_char_occurrences_in_str(consonants, string)
 
 
 def calculate_char_occurrences_in_str(characters, string):
+    """
+    Calculates the occurrences of the input characters
+    in the input string
+    :param characters: characters to count
+    :param string: input string
+    :return: the count of all input characters in the string
+    """
     occurrences = 0
     string = string.casefold()
     for character in characters:
@@ -43,6 +76,11 @@ def calculate_char_occurrences_in_str(characters, string):
 
 
 def prime_number(number):
+    """
+    Checks if a number is prime
+    :param number: input number
+    :return: True if number is prime, False otherwise
+    """
     for i in range(2, number - 1):
         if number % i == 0:
             return False
@@ -50,6 +88,13 @@ def prime_number(number):
 
 
 def fact_digits(number):
+    """
+    Calculates the sum of the factorials
+    for each digit of number
+    :param number: input number
+    :return: the sum of the factorials
+    for each digit of number
+    """
     sum_fact = 0
     for digit in to_digits(number):
         sum_fact += fact(digit)
@@ -57,6 +102,11 @@ def fact_digits(number):
 
 
 def fact(number):
+    """
+    Calculates the factorial of a number
+    :param number: input number
+    :return: The factorial of number
+    """
     result = 1
     for number in range(1, number + 1):
         result *= number
@@ -64,6 +114,11 @@ def fact(number):
 
 
 def fibonacci(elements_num):
+    """
+    Calculates Fibonacci sequence for elements_num members
+    :param elements_num: number of sequence members
+    :return: Fibonacci sequence as list
+    """
     sequence = []
     for i in range(1, elements_num + 1):
         if i <= 2:
@@ -75,6 +130,11 @@ def fibonacci(elements_num):
 
 
 def fib_number(elements_num):
+    """
+    Calculates number based on members of Fibonacci sequence
+    :param elements_num: number of sequence members
+    :return: Number formed by concatenation of the sequence members
+    """
     string = ""
     for number in fibonacci(elements_num):
         string += str(number)
@@ -82,11 +142,23 @@ def fib_number(elements_num):
 
 
 def palindrome(input):
+    """
+    Checks if the input is palindrome
+    :param input:
+    :return: True if the input is palindrome, else False
+    """
     input = str(input)
     return input[::-1] == input
 
 
 def char_histogram(string):
+    """
+    Returns a map with characters of the string as keys
+    and their occurrences as values
+    :param string: input string
+    :return: Map populated with characters of string as keys
+    and the occurrences of the characters in the string as values
+    """
     result = {}
     for char in string:
         result[char] = string.count(char)
