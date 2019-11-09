2e3dbfab63cfe1cd558ced93da19a8713a68fd3b
Rosen Sasov <rosen.sasov@mail.com>
2019-11-09 12:11:49 +0200

    Fix remarks

--- module04/02-Bank-Account/solution.py
+++ module04/02-Bank-Account/solution.py
@@ -11,33 +11,35 @@ class BankAccount:
         self.account_history = ["Account was created"]
 
     def __str__(self):
-        return "Bank account for {} with balance of {} {}".format(self.name, self.account_balance, self.currency)
+        return "Bank account for %s with balance of %s%s" % (self.name, self.account_balance, self.currency)
 
     def __int__(self):
-        self.account_history.append("__int__ check -> {}$".format(self.account_balance))
+        self.account_history.append("__int__ check -> %s%s" % (self.account_balance, self.currency))
         return self.account_balance
 
     def deposit(self, amount):
         if amount < 0:
             raise ValueError("Negative amount for deposit is not supported")
         self.account_balance += amount
-        self.account_history.append("Deposited {}$".format(amount))
+        self.account_history.append("Deposited %s%s" % (amount, self.currency))
 
     def balance(self):
-        self.account_history.append("Balance check -> {}$".format(self.account_balance))
+        self.account_history.append("Balance check -> %s%s" % (self.account_balance, self.currency))
         return self.account_balance
 
     def withdraw(self, amount):
         if not self.account_balance > amount:
-            self.account_history.append("Withdraw for {}$ failed".format(amount))
+            self.account_history.append("Withdraw for %s%s failed" % (amount, self.currency))
             return False
         self.account_balance -= amount
-        self.account_history.append("{}$ was withdrawn".format(amount))
+        self.account_history.append("%s%s was withdrawn" % (amount, self.currency))
         return True
 
     def transfer_to(self, account, amount):
-        if not (self.currency == account.currency and self.account_balance > amount):
-            return False
+        if not self.currency == account.currency:
+            raise TypeError("Transfer between accounts with different currencies not supported")
+        if not self.account_balance > amount:
+            raise TypeError("Insufficient funds")
         account.deposit(amount)
         self.account_balance -= amount
         return True
