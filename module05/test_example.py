6450014647c90dd3af572292a71c1fa7d2139669
Rosen Sasov <rosen.sasov@mail.com>
2019-11-17 14:08:05 +0200

    Cosmetic changes

--- module05/test_example.py
+++ module05/test_example.py
@@ -1,33 +1,34 @@
 import unittest
 from datetime import datetime
 
+
 class ExampleTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print "setUpClass executes only once. CREATE DATABASE;"
+        print("setUpClass executes only once. CREATE DATABASE;")
         print
 
     def setUp(self):
-        print "    setUp executes before every test method!"
+        print("    setUp executes before every test method!")
 
     def tearDown(self):
-        print "    tearDown executes after every test method!"
+        print("    tearDown executes after every test method!")
 
     @classmethod
     def tearDownClass(cls):
         print
-        print "tearDownClass executes only once. DROP DATABASE;"
+        print("tearDownClass executes only once. DROP DATABASE;")
 
     def test_first_method(self):
-        print "        This is the first test. It will PASS"
+        print("        This is the first test. It will PASS")
         assert True
 
     def test_second_method(self):
-        print "        This is the second test. It will FAIL"
+        print("        This is the second test. It will FAIL")
         assert False
 
     def test_zzzzz(self):
-        print "        This is the last test. It will PASS"
+        print("        This is the last test. It will PASS")
         assert True
 
 
@@ -41,6 +42,7 @@ class FlakyTest(unittest.TestCase):
 
     def test_myself(self):
         self.do_fail()
+
 
 if __name__ == "__main__":
     unittest.main()
